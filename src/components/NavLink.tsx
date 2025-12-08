c68a067cc188af588ff14246d1b8623b92cac7e6
yvonnegichovi <yvonnegichovi@gmail.com>
2025-12-08 17:43:35 +0300

    fix: Removed trailing comma

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,6 +1,6 @@
-import { NavLink as RouterNavLink, type NavLinkProps } from "react-router-dom";
-import { cn } from "@/lib/utils";
+import { NavLink as RouterNavLink, NavLinkProps } from "react-router-dom";
 import { forwardRef } from "react";
+import { cn } from "@/lib/utils";
 
 interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
   className?: string;
@@ -9,31 +9,18 @@ interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
 }
 
 const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
-  (
-    {
-      className,
-      activeClassName,
-      pendingClassName,
-      to,
-      ...props,
-    },
-    ref
-  ) => {
+  ({ className, activeClassName, pendingClassName, to, ...props }, ref) => {
     return (
       <RouterNavLink
-        to={to}
         ref={ref}
+        to={to}
         className={({ isActive, isPending }) =>
-          cn(
-            className,
-            isActive && activeClassName,
-            isPending && pendingClassName
-          )
+          cn(className, isActive && activeClassName, isPending && pendingClassName)
         }
         {...props}
       />
     );
-  }
+  },
 );
 
 NavLink.displayName = "NavLink";
